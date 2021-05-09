edd3fa5715fbe5f89ef9a3eb5c5ef850205488bf
Daepoid <daepoid@gmail.com>
2021-05-09 17:53:16 +0900

    Update 16956

--- PS_2021/PS_2021/15001~20000/16956.cpp
+++ PS_2021/PS_2021/15001~20000/16956.cpp
@@ -2,62 +2,79 @@
 #define MAX 501
 using namespace std;
 
-struct go {
-  int x;
-  int y;
-};
-
-int n, m;
+int R, C;
+int dy[4] = {0, -1, 0, 1};
+int dx[4] = {1, 0, -1, 0};
 char board[MAX][MAX];
-vector<go> v;
+bool flag = false;
+bool visited[MAX][MAX];
+vector<pair<int, int>> v;
 
-int dy[4] = {-1, 0, 1, 0};
-int dx[4] = {0, -1, 0, 1};
+bool isvalid(int newy, int newx) {
+  return 0 <= newy && newy < R && 0 <= newx && newx < C;
+}
+
+bool bfs(int a, int b) {
+  queue<pair<int, int>> q;
+  q.push({a, b});
+  visited[a][b] = true;
+
+  while (!q.empty()) {
+    int y = q.front().first;
+    int x = q.front().second;
+    q.pop();
+
+    if (board[y][x] == 'S') {
+      return true;
+    }
+
+    for (int i = 0; i < 4; i++) {
+      int newy = y + dy[i];
+      int newx = x + dx[i];
+
+      if (!isvalid(newy, newx)) {
+        continue;
+      }
+      if (board[newy][newx] == 'D') {
+        continue;
+      }
+      if (board[newy][newx] == 'W') {
+        continue;
+      }
+      if (visited[newy][newx]) {
+        continue;
+      }
+      visited[newy][newx] = true;
+      q.push({newy, newx});
+    }
+  }
+  return false;
+}
 
 int main() {
-  scanf("%d%d", &n, &m);
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
+  scanf("%d %d", &R, &C);
+  for (int i = 0; i < R; i++) {
+    for (int j = 0; j < C; j++) {
       scanf(" %c", &board[i][j]);
       if (board[i][j] == 'W') {
         v.push_back({i, j});
-      }
-    }
-  }
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      if (board[i][j] == '.') {
+      } else if (board[i][j] == '.') {
         board[i][j] = 'D';
       }
     }
   }
-  int flag = 0;
-  for (int i = 0; i < v.size(); i++) {
-    int r = v[i].x;
-    int c = v[i].y;
-    for (int j = 0; j < 4; j++) {
-      int nr = r + dy[j];
-      int nc = c + dx[j];
-      if (nr < 0 || nc < 0 || nr >= n || nc >= m)
-        continue;
-      if (board[nr][nc] == 'S') {
-        flag = 1;
-        break;
-      }
-    }
-    if (flag == 1)
-      break;
-  }
 
-  if (flag == 0) {
-    cout << "1" << endl;
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        cout << board[i][j];
-      }
-      cout << endl;
+  for (int i = 0; i < v.size(); i++) {
+    flag = bfs(v[i].first, v[i].second);
+    if (flag) {
+      printf("0\n");
+      return 0;
     }
-  } else {
-    cout << "0" << endl;
   }
+  printf("1\n");
+  for (int i = 0; i < R; i++) {
+    board[i][C] = '\0';
+    printf("%s\n", board[i]);
+  }
+  return 0;
 }
