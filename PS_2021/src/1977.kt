0c7f3c83f778281637fd64aef2f2f7dfe3ef1205
Daepoid <daepoid@gmail.com>
2021-06-22 16:40:59 +0900

    Add files

---
+++ PS_2021/src/1977.kt
@@ -0,0 +1,37 @@
+import java.util.*
+import kotlin.math.*
+
+class Q1977 {
+
+}
+
+fun main() {
+    val input: Scanner = Scanner(System.`in`)
+    var M: Int = input.nextInt()
+    var N: Int = input.nextInt()
+    var sum: Int = 0
+    var min_num: Int = 20000
+
+    if (M > N){
+        M = N.also {N = M}
+    }
+
+    var numList: MutableList<Int> = mutableListOf<Int>()
+
+    for(i: Int in 0..100){
+        numList.add(i * i)
+    }
+
+    for(i: Int in 0..100){
+        if(M <= numList[i] && numList[i] <= N){
+            sum += numList[i]
+            min_num = min(min_num, numList[i])
+        }
+    }
+
+    if(sum == 0) {
+        println(-1)
+    } else {
+        println("$sum\n$min_num")
+    }
+}
