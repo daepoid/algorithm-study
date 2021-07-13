d8f73fff013b12199d529db9c2cf842db22faae4
Daepoid <daepoid@gmail.com>
2021-07-13 22:26:39 +0900

    Update Files

--- PS_2021/PS_2021/15001~20000/21611_3.cpp
+++ PS_2021/PS_2021/15001~20000/21611_3.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
-#define MAX 50
+#define MAX 60
 using namespace std;
 
 int N, M;
-int board[MAX][MAX];
+int input_board[MAX][MAX];
 int dy[4] = {0, 1, 0, -1};
 int dx[4] = {-1, 0, 1, 0};
 int sum[3] = {0, 0, 0};
 vector<pair<int, int>> cmds;
-vector<int> list_board;
+vector<int> board;
 
 bool isvalid(int newy, int newx) {
   return 0 <= newy && newy < N && 0 <= newx && newx < N;
@@ -18,19 +18,19 @@ void make_list_board() {
   int y = N / 2;
   int x = N / 2;
   int dir = 0;
-  list_board.push_back(0);
-  for (int i = 1; i < N; i++) {
+  board.push_back(0);
+  for (int i = 1; i < N + 2; i++) {
     for (int j = 0; j < 2; j++) {
       for (int k = 0; k < i; k++) {
         if (!isvalid(y, x)) {
-          printf("Error\n");
           return;
         }
         y += dy[dir];
         x += dx[dir];
-        if (board[y][x] != 0) {
-          list_board.push_back(board[y][x]);
+        if (input_board[y][x] == 0) {
+          return;
         }
+        board.push_back(input_board[y][x]);
       }
       dir = (dir + 1) % 4;
     }
@@ -44,19 +44,19 @@ void blizard(int idx) {
 
   for (int i = s; i > 0; i--) {
     int pos = 4 * i * i + (weight[d] - 4) * i;
-    list_board.erase(list_board.begin() + pos);
+    board.erase(board.begin() + pos);
   }
 }
 
 bool is_boomable() {
   int cnt = 1;
-  for (int i = 1; i < list_board.size() - 1; i++) {
-    if (list_board[i] == list_board[i + 1] && list_board[i] != 0) {
+  for (int i = 1; i < board.size() - 1; i++) {
+    if (board[i] == board[i + 1]) {
       cnt++;
       if (cnt >= 4) {
         return true;
       }
-    } else if (list_board[i] != list_board[i + 1]) {
+    } else if (board[i] != board[i + 1]) {
       cnt = 1;
     }
   }
@@ -66,32 +66,57 @@ bool is_boomable() {
 void boom_marbles() {
   int cnt = 1;
   vector<pair<int, int>> v;
-  for (int i = list_board.size() - 1; i > 1; i--) {
-    if (list_board[i] == list_board[i - 1]) {
+  for (int i = board.size() - 1; i > 1; i--) {
+    if (board[i] == board[i - 1]) {
       cnt++;
-    } else if (list_board[i] != list_board[i - 1]) {
+    } else if (board[i] != board[i - 1]) {
       if (cnt >= 4) {
-        sum[list_board[i] - 1] += cnt;
+        sum[board[i] - 1] += cnt;
         v.push_back({i, cnt});
       }
       cnt = 1;
     }
   }
+  if (cnt >= 4) {
+    sum[board[1] - 1] += cnt;
+    v.push_back({1, cnt});
+  }
   for (int i = 0; i < v.size(); i++) {
     int pos = v[i].first;
-    list_board.erase(list_board.begin() + pos,
-                     list_board.begin() + pos + v[i].second);
-    // for (int j = 0; j < v[i].second; j++) {
-    //   list_board.erase(list_board.begin() + v[i].first);
-    // }
+    board.erase(board.begin() + pos, board.begin() + pos + v[i].second);
   }
+}
+
+void remake_board() {
+  vector<int> temp;
+  temp.push_back(0);
+
+  int cnt = 1;
+  for (int i = 1; i < board.size() - 1; i++) {
+    if (board[i] == board[i + 1]) {
+      cnt++;
+    } else {
+      temp.push_back(cnt);
+      temp.push_back(board[i]);
+      cnt = 1;
+    }
+  }
+  temp.push_back(cnt);
+  temp.push_back(board[board.size() - 1]);
+
+  if (temp.size() > N * N - 1) {
+    temp.erase(temp.begin() + N * N - 1, temp.end());
+  }
+
+  board.clear();
+  board = temp;
 }
 
 int main() {
   scanf("%d %d", &N, &M);
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
-      scanf("%d", &board[i][j]);
+      scanf("%d", &input_board[i][j]);
     }
   }
   for (int i = 0; i < M; i++) {
@@ -106,6 +131,9 @@ int main() {
     while (is_boomable()) {
       boom_marbles();
     }
+    remake_board();
   }
+
   printf("%d\n", sum[0] + sum[1] * 2 + sum[2] * 3);
+  return 0;
 }
