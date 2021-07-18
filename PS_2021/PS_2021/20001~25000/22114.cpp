b26fa8b509e20b3fbfffa7541ff2683b9e2c2233
Daepoid <daepoid@gmail.com>
2021-07-18 10:23:19 +0900

    Add Files

--- PS_2021/PS_2021/20001~25000/22114.cpp
+++ PS_2021/PS_2021/20001~25000/22114.cpp
@@ -2,9 +2,8 @@
 #define MAX 100100
 using namespace std;
 
-int N, K;
-int L[MAX];
-int dp[MAX][2];
+int N, K, answer = 1;
+int L[MAX], dp[MAX][2];
 
 int main() {
   scanf("%d %d", &N, &K);
@@ -15,16 +14,15 @@ int main() {
   dp[1][1] = 1;
 
   for (int i = 1; i < N; i++) {
-    int sum = 0;
-    for (int j = i; j < N; j++) {
-      sum += L[j];
-      if (sum > K) {
-        dp[j + 1][1] = max(dp[j + 1][1], dp[i][0] + 1);
-        break;
-      }
-      dp[j + 1][0] = max(dp[j + 1][0], dp[i][0] + 1);
-      dp[j + 1][1] = max(dp[j + 1][1], dp[i][1] + 1);
+    if (L[i] <= K) {
+      dp[i + 1][0] = max(dp[i + 1][0], dp[i][0] + 1);
+      dp[i + 1][1] = max(dp[i + 1][1], dp[i][1] + 1);
+    } else {
+      dp[i + 1][0] = 1;
+      dp[i + 1][1] = max(dp[i + 1][1], dp[i][0] + 1);
     }
+    answer = max(answer, dp[i + 1][0]);
+    answer = max(answer, dp[i + 1][1]);
   }
-  printf("%d\n", max(dp[N][0], dp[N][1]));
+  printf("%d\n", answer);
 }
