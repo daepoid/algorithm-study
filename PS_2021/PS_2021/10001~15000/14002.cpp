2039ee1e542b3d7cd2a55d9f7cda7ed4fc3d172d
Daepoid <daepoid@gmail.com>
2021-07-22 13:55:51 +0900

    Add Files

---
+++ PS_2021/PS_2021/10001~15000/14002.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define MAX 1001
+using namespace std;
+
+int N;
+int board[MAX];
+
+int main() {
+  scanf("%d", &N);
+  for (int i = 0; i < N; i++) {
+    scanf("%d", &board[i]);
+  }
+}
